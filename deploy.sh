ffc8a2cf381a68a0b8274f7c00fe99373f17b529
forestdan <dandancc007@qq.com>
2020-11-24 14:26:06 +0900

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -ex
+
+target_branch="br-dev"
+git config --global user.name "CircleCI deployer"
+git config --global user.email "<>"
+git checkout $target_branch
+
+echo "$(python getDeployTime.py)" > foo.txt
+
+git add foo.txt
+git commit -m "[skip ci] updates GitHub Pages"
+if [ $? -ne 0 ]; then
+  echo "nothing to commit"
+  exit 0
+fi
+
+git remote set-url origin "git@github.com:forestdan/myproj.git"
+git push -f origin $target_branch
